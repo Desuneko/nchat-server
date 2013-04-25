db8c472ee9912da37e144f41c3d146fd76746dd5
Desuneko <onexxen@gmail.com>
2013-04-25 18:58:31 +0200

    Added userlist

--- ClientThread.java
+++ ClientThread.java
@@ -6,7 +6,7 @@ import java.net.*;
 public class ClientThread extends Thread {
 	Socket clientSocket;
 	boolean noErrors = true;
-	String username = "";
+	public String username = "";
 	String serverId = "";
 	int protocolVersion = 0;
 	OutputStream outs;
@@ -18,11 +18,15 @@ public class ClientThread extends Thread {
     ClientThread(Socket client) {
         this.clientSocket = client;
     }
-
     public synchronized boolean sendMessage(String msg)
     {
+    	return sendMessage(msg, (byte)0x04);
+    
+    }
+    public synchronized boolean sendMessage(String msg, byte type)
+    {
     	try {
-			put.write((byte) 0x04);
+			put.write((byte) type);
 			put.writeShort(msg.length());
 			put.writeChars(msg);
 			put.flush();
@@ -34,18 +38,7 @@ public class ClientThread extends Thread {
 		}
     }
     
-    public void broadcastMessage(String msg)
-    {
-    	for (ClientThread cThread : Server.clientThreads)
-		{
-			boolean result = cThread.sendMessage(msg);
-			if (result == false)
-			{
-				Server.clientThreads.remove(cThread);
-			}
-		}	
-    	
-    }
+   
     
     public void run() {
     	try {
@@ -55,13 +48,38 @@ public class ClientThread extends Thread {
     		ins = clientSocket.getInputStream();
     		in = new DataInputStream(ins);
 			int packetId = 0;
+			long lastReadTime = System.currentTimeMillis();
 		    while(noErrors)
 		    {
+		    	if ((clientSocket.isClosed()) || (!clientSocket.isConnected()))
+		    	{
+		    		if (username != "")
+		    		{
+		    			String kick_msg = "You have been disconnected from server.";
+		    			put.write((byte) 0xFF);
+						put.writeShort(kick_msg.length());
+						put.writeChars(kick_msg);
+						put.flush();
+		    			Server.clientList.remove(username);
+		    			Server.broadcastMessage("** "+username+" has disconnected from server");
+		    			Server.clientThreads.remove(this);
+		    		}
+		    		noErrors = false;
+		    		break;
+		    	}
 		    	
+		    	if ((System.currentTimeMillis() - lastReadTime) > 2000)
+		    	{
+		    		//System.out.println("Sent keepalive");
+		    		put.write((byte) 0x01);
+					put.flush();
+					lastReadTime = System.currentTimeMillis();
+					
+		    	}
 		    	if (ins.available() > 0)
 		    	{
 		    		packetId = ins.read();
-				    System.out.println("Received packet id - "+packetId);
+				    //System.out.println("Received packet id - "+packetId);
 		    		
 					switch(packetId)
 					{
@@ -84,11 +102,39 @@ public class ClientThread extends Thread {
 							{
 								username += in.readChar();
 							}
-							
+							Server.clientList.remove(username);
+							Server.clientList.add(username);
 							this.sendMessage(Server.welcome);
-							broadcastMessage("** "+username+" has joined the server");
-							
-							
+							Server.broadcastMessage("** "+username+" has joined the server");
+							Server.broadcastMessage(username, (byte)0x06);
+							put.write((byte) 0x04);
+							String userString = "";
+							for (String username : Server.clientList)
+							{
+								if (userString == "")
+								{
+									userString = username;
+								} else {
+									userString = userString + ", " + username;
+								}
+							}
+							userString = "Connected users: "+userString;
+							put.writeShort(userString.length());
+							put.writeChars(userString);
+							put.flush();
+							put.write((byte) 0x05);
+							userString = "";
+							for (String username : Server.clientList)
+							{
+								if (userString == "")
+								{
+									userString = username;
+								} else {
+									userString = userString + "\0" + username;
+								}
+							}
+							put.writeShort(userString.length());
+							put.writeChars(userString);
 							put.flush();
 							break;
 						case 0x03:
@@ -98,8 +144,29 @@ public class ClientThread extends Thread {
 							{
 								msg += in.readChar();
 							}
-							broadcastMessage("<"+username+">"+msg);
-							break;
+							switch (msg)
+							{
+								case "/who":
+									put.write((byte) 0x04);
+									String userStrings = "";
+									for (String username : Server.clientList)
+									{
+										if (userStrings == "")
+										{
+											userStrings = username;
+										} else {
+											userStrings = userStrings + ", " + username;
+										}
+									}
+									userStrings = "Connected users: "+userStrings;
+									put.writeShort(userStrings.length());
+									put.writeChars(userStrings);
+									put.flush();
+									break;
+								default:
+									Server.broadcastMessage("<"+username+">"+msg);
+									break;
+							}
 					}
 		    	}
 		    }
@@ -110,9 +177,27 @@ public class ClientThread extends Thread {
 		    outs.close();
 		    clientSocket.close();
 		    Server.clientThreads.remove(this);
+    	} catch (EOFException e) {
+    		if (username != "")
+    		{
+    			noErrors = false;
+    			Server.clientList.remove(username);
+    			Server.broadcastMessage("** "+username+" has disconnected from server");
+    			Server.broadcastMessage(username, (byte)0x07);
+    			Server.clientThreads.remove(this);
+    		}
+		} catch (SocketException e) {
+    		if (username != "")
+    		{
+    			noErrors = false;
+    			Server.clientList.remove(username);
+    			Server.broadcastMessage("** "+username+" has disconnected from server");
+    			Server.broadcastMessage(username, (byte)0x07);
+    			Server.clientThreads.remove(this);
+    		}
 		} catch (Exception e) {
 		    System.out.println("We haz a problem :C (someone disconnected?)");
-		    //e.printStackTrace();
+		    e.printStackTrace();
 		}
     	
     }
--- Server.java
+++ Server.java
@@ -11,10 +11,28 @@ public class Server {
 	public static ArrayList<ClientThread> clientThreads = new ArrayList<ClientThread>();
 	public static String motd = "§1"+"\0"+"1"+"\0"+"0.5.0"+"\0"+"nchat test server"+"\0"+"0"+"\0"+"0";
 	public static String welcome = "Welcome on ncraft test server";
+	public static ArrayList<String> clientList = new ArrayList<String>();
 	// 
 	/**
 	 * @param args
 	 */
+	
+	public static String[] listUsers()
+	{
+		ArrayList<String> users = new ArrayList<String>();
+		for (ClientThread cThread : Server.clientThreads)
+		{
+			
+			if (cThread.clientSocket.isClosed())
+			{
+				
+				Server.clientThreads.remove(cThread);
+			}
+			users.add(cThread.username);
+		}
+		return (String[]) users.toArray();
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
@@ -34,5 +52,49 @@ public class Server {
 		    System.exit(-1);
 		}
 	}
+	public static void broadcastMessage(String msg)
+	{
+		broadcastMessage(msg, (byte)0x04);
+	}
+	 public static void broadcastMessage(String msg, byte type)
+	    {
+		 if (type==0x04)
+		 {
+			 System.out.println(msg);
+		 }
+	    	for (ClientThread cThread : clientThreads)
+			{
+	    		if (cThread.noErrors == true)
+	    		{
+		    		try 
+		    		{
+		    			if (cThread.clientSocket.isClosed())
+						{
+							clientThreads.remove(cThread);
+						}
+						boolean result = cThread.sendMessage(msg, type);
+						if (result == false)
+						{
+							clientThreads.remove(cThread);
+						}
+		    			
+		    		} catch (Exception e)
+		    		{
+		    			try 
+			    		{
+			    			clientThreads.remove(cThread);
+			    			
+			    		} catch (Exception e2)
+			    		{
+							
+							
+			    			
+			    		}
+		    		}
+	    		}
+				
+			}	
+	    	
+	    }
 
 }
